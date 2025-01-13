eaae62d64286cdbf9a4abfcca717f712d04c95a5
KevinLo9231 <kevinlo9231@gmail.com>
2025-01-13 22:44:27 +0800

    完成ping pong回答

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { Client, IntentsBitField } = require('discord.js');
+
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', () => {
+    console.log('Bot is ready');
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
