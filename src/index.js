83ca95604aa2a2b0f03f6f1115d3288d9b7c275a
KevinLo9231 <kevinlo9231@gmail.com>
2025-01-22 23:56:21 +0800

    新增「今天的天氣」指令，整合氣象API以回覆指定城市的天氣狀況，並更新指令選項

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { Client, IntentsBitField } = require('discord.js');
+const axios = require('axios');
 require('dotenv').config();
 
 const client = new Client({
@@ -20,14 +21,38 @@ client.on('ready', () => {
     // }
 // });
 
-client.on('interactionCreate', (interaction) => {
+client.on('interactionCreate',(interaction) => {
     if (!interaction.isCommand()) return;
     if (interaction.commandName === 'hey') {
         interaction.reply('hey');
     }
-    if (interaction.commandName === 'location') {
+    if (interaction.commandName === '今天的天氣') {
         const city = interaction.options.get('city').value;
-        interaction.reply('You selected ' + city);
+
+        axios.get('https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001', {
+            params: {
+                Authorization: process.env.CWA_API_KEY,
+            }
+        })
+            .then((response) => {
+                console.log(response.data);
+                const locationData = response.data.records.location;
+                const cityData = locationData.find((data) => data.locationName === city);
+                console.log(cityData);
+                if (cityData) {
+                    interaction.reply(
+                        `今天${city}的天氣狀況: ${cityData.weatherElement[0].time[0].parameter.parameterName}\n` +
+                        `降雨機率: ${cityData.weatherElement[1].time[0].parameter.parameterName}%\n` +
+                        `溫度: ${cityData.weatherElement[2].time[0].parameter.parameterName}°C ~ ${cityData.weatherElement[4].time[0].parameter.parameterName}°C\n`
+                    );
+                } else {
+                    interaction.reply('City not found');
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                interaction.reply('Error fetching weather data');
+            });
     }
     console.log(interaction);
 });
--- src/register-commands.js
+++ src/register-commands.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 // 註冊 discord slash commands
 const commands = [
     {
-        name: 'location',
-        description: 'Replies with location',
+        name: '今天的天氣', // 改為小寫
+        description: '今天的天氣狀況',
         options: [
             {
                 name: 'city',
@@ -14,17 +14,93 @@ const commands = [
                 required: true,
                 choices: [
                     {
-                        name: 'Taipei',
-                        value: 'Taipei'
+                        name: '宜蘭縣',
+                        value: '宜蘭縣'
                     },
                     {
-                        name: 'Taoyuan',
-                        value: 'Taoyuan'
+                        name: '花蓮縣',
+                        value: '花蓮縣'
                     },
                     {
-                        name: 'Hsinchu',
-                        value: 'Hsinchu'
+                        name: '臺北市',
+                        value: '臺北市'
                     },
+                    {
+                        name: '新北市',
+                        value: '新北市'
+                    },
+                    {
+                        name: '桃園市',
+                        value: '桃園市'
+                    },
+                    {
+                        name: '臺中市',
+                        value: '臺中市'
+                    },
+                    {
+                        name: '臺南市',
+                        value: '臺南市'
+                    },
+                    {
+                        name: '高雄市',
+                        value: '高雄市'
+                    },
+                    {
+                        name: '基隆市',
+                        value: '基隆市'
+                    },
+                    {
+                        name: '新竹縣',
+                        value: '新竹縣'
+                    },
+                    {
+                        name: '新竹市',
+                        value: '新竹市'
+                    },
+                    {
+                        name: '嘉義市',
+                        value: '嘉義市'
+                    },
+                    {
+                        name: '苗栗縣',
+                        value: '苗栗縣'
+                    },
+                    {
+                        name: '彰化縣',
+                        value: '彰化縣'
+                    },
+                    {
+                        name: '南投縣',
+                        value: '南投縣'
+                    },
+                    {
+                        name: '雲林縣',
+                        value: '雲林縣'
+                    },
+                    {
+                        name: '嘉義縣',
+                        value: '嘉義縣'
+                    },
+                    {
+                        name: '屏東縣',
+                        value: '屏東縣'
+                    },
+                    {
+                        name: '臺東縣',
+                        value: '臺東縣'
+                    },
+                    {
+                        name: '澎湖縣',
+                        value: '澎湖縣'
+                    },
+                    {
+                        name: '金門縣',
+                        value: '金門縣'
+                    },
+                    {
+                        name: '連江縣',
+                        value: '連江縣'
+                    }
                 ],
             },
         ],
